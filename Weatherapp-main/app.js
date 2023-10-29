086d1603e8c1acc95e77b28b0b9d83d1f2980be1
Anirudh negi <103302296+anirudhnegi12@users.noreply.github.com>
2023-10-29 11:50:43 +0530

    Add files via upload

---
+++ Weatherapp-main/app.js
@@ -0,0 +1,39 @@
+const API_KEY = "6d055e39ee237af35ca066f35474e9df"; // Replace with your actual API key
+
+const form = document.querySelector("form");
+const search = document.querySelector("#search");
+const weather = document.querySelector("#weather");
+
+const getWeather = async (city) => {
+  weather.innerHTML = `<h2>LOAD HO RHA HAI</h2>`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  const response = await fetch(url);
+  //   console.log(response);
+  const data = await response.json();
+  //   console.log(data);
+  return showWeather(data);
+};
+const showWeather = function (data) {
+  if (data.cod == "404") {
+    weather.innerHTML = `<h2>CITY NOT FOUND</h2>`;
+    return;
+  }
+  weather.innerHTML = `<div>
+<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+</div>
+<div>
+<h2>${data.main.temp}</h2>
+<h4> ${data.weather[0].main}</h4>
+</div>`;
+};
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const city = search.value;
+  getWeather(city);
+});
+document.querySelector("button").addEventListener("click", function (event) {
+  event.preventDefault();
+  const city = search.value;
+  getWeather(city);
+});
